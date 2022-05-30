343b2f50534f8a2f412ae995198da8fbaaa0606a
Sergii <rumabasy@gmail.com>
2022-05-30 03:48:18 +0300

    add index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=	, initial-scale=1.0">
+	<title>git</title>
+</head>
+<body>
+	<ol>
+		<li>one</li>
+		<li>two</li>
+		<li>three</li>
+	</ol>
+</body>
+</html>
