1adb535946bb708e13050f4f715625c2d5bb392b
rumasyba <rumasyba@gmail.com>
2023-03-08 13:01:48 +0300

    add index for check netbeans82 new

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=	, initial-scale=1.0">
-	<title>git</title>
-</head>
-<body>
-	<ol> MVC
-		<li>Model</li>
-		<li>View</li>
-		<li>Controller</li>
-	</ol>
-</body>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo 'gitTutorial';
+      echo 'try netbeans 8.2 git';
+    ?>
+  </body>
 </html>
